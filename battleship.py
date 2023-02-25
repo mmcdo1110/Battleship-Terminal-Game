0b069c0e5ada57d6d0549eb3fbbadc1abdfb41d0
mmcdon96 <mmcdon96@gmail.com>
2023-02-24 23:01:49 -0600

    Terminal inputs to enter and assign player 1 and player 2 names to class objects.

--- battleship.py
+++ battleship.py
@@ -2,7 +2,11 @@ class Player:
     def __init__(self, name):
         self.name = name
 
-player1 = input("Please enter the name of Player 1")
+#Define player 1 and 2 names through terminal inputs.
+player1_name = input("Please enter the name of player 1: ")
+player1 = Player(player1_name)
+player2_name = input("Please enter the name of player 2: ")
+player2 = Player(player2_name)
 
 def print_blank_grid():
     for r in range(10):
