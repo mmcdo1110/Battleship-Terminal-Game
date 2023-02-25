d71ff500b29d20d4bd584a09f68328b7fa662585
mmcdon96 <mmcdon96@gmail.com>
2023-02-24 21:43:26 -0600

    Created function that prints a blank grid.

--- battleship.py
+++ battleship.py
@@ -3,3 +3,12 @@ class Player:
         self.name = name
 
 player1 = input("Please enter the name of Player 1")
+
+def print_blank_grid():
+    for r in range(10):
+        row = ""
+        for c in range(10):
+            row += "-"
+        print(row)
+
+print_blank_grid()
